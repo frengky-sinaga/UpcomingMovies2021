43495f334c3361141991695d5c000996d7800c0b
Frengky <fsinaga9c@gmail.com>
2021-06-04 22:08:08 +0700

    code to detailFragment

--- app/src/main/java/com/dicoding/upcomingmovies2021/ui/fragments/DetailFragment.kt
+++ app/src/main/java/com/dicoding/upcomingmovies2021/ui/fragments/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import coil.load
 import com.dicoding.upcomingmovies2021.R
 import com.dicoding.upcomingmovies2021.databinding.FragmentDetailBinding
 import com.dicoding.upcomingmovies2021.ui.viewmodel.FilmViewModel
@@ -19,7 +20,9 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         viewModel = ViewModelProvider(requireActivity()).get(FilmViewModel::class.java)
 
         viewModel.getDataFilm().observe(viewLifecycleOwner, { data ->
-            binding.title.text = data.title
+            binding.tvTitle.text = data.title
+            binding.tvDescription.text = data.description
+            binding.imgPoster.load(data.poster)
         })
     }
 }
