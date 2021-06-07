213961b2a3d3aa3cdeb0c834b5e42379e014e7d3
Frengky <fsinaga9c@gmail.com>
2021-06-07 17:41:37 +0700

    revision 2: ui and instrument test

--- app/src/androidTest/java/com/dicoding/upcomingmovies2021/ui/fragments/MainActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/upcomingmovies2021/ui/fragments/MainActivityTest.kt
@@ -2,8 +2,7 @@ package com.dicoding.upcomingmovies2021.ui.fragments
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -36,24 +35,22 @@ class MainActivityTest {
     @Test
     fun loadTvShow() {
         onView(allOf(withId(R.id.viewPager), isDisplayed())).perform(swipeLeft())
-        onView(allOf(withId(R.id.rv_film), isDisplayed()))
-            .perform(
-                RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                    dummyTvShow.size
-                )
+        onView(allOf(withId(R.id.rv_film), isDisplayed())).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyTvShow.size
             )
+        )
     }
 
     @Test
-    fun viewDetail() {
+    fun viewDetailTvShow() {
         onView(allOf(withId(R.id.viewPager), isDisplayed())).perform(swipeLeft())
-        onView(allOf(withId(R.id.rv_film), isDisplayed()))
-            .perform(
-                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                    0,
-                    click()
-                )
+        onView(withId(R.id.rv_film)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
             )
+        )
 
         onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_title)).check(matches(withText(dummyTvShow[0].title)))
@@ -76,5 +73,28 @@ class MainActivityTest {
         if (!creator) {
             onView(withId(R.id.chip_creators)).check(matches(isDisplayed()))
         }
+    }
+
+    @Test
+    fun viewDetailMovie() {
+        onView(allOf(withId(R.id.rv_film), isDisplayed()))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    0,
+                    click()
+                )
+            )
+
+        onView(allOf(withId(R.id.fragment_detail), isDisplayed())).perform(swipeDown())
+        onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title)).check(matches(withText(dummyMovie[0].title)))
+        onView(withId(R.id.img_poster)).check(matches(isDisplayed()))
+        onView(withId(R.id.img_icon)).check(matches(isDisplayed()))
+        onView(withId(R.id.chip_genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.content_description)).check(matches(withText(dummyMovie[0].description)))
+        onView(withId(R.id.tv_release_date)).check(matches(withText(dummyMovie[0].releaseDate)))
+        onView(withId(R.id.chip_stars)).check(matches(isDisplayed()))
+        onView(withId(R.id.chip_directors)).check(matches(isDisplayed()))
+        onView(withId(R.id.chip_writers)).check(matches(isDisplayed()))
     }
 }
