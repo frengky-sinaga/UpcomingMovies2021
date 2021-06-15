2d497f0666e038ce8d6c152987e3de20c058a3c0
Frengky <fsinaga9c@gmail.com>
2021-06-16 01:01:58 +0700

    adding constants and libraries

---
+++ app/src/main/java/com/dicoding/upcomingmovies2021/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.dicoding.upcomingmovies2021.utils
+
+import com.dicoding.upcomingmovies2021.BuildConfig
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = BuildConfig.API_KEY
+    const val API_POSTER_PATH = "https://image.tmdb.org/t/p/w300"
+    const val API_BACKDROP_PATH = "https://image.tmdb.org/t/p/w500"
+}
