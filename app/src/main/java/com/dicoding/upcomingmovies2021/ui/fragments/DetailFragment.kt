19f150084400a0b9fc0b11b2c7f40e65b3aec249
Frengky <fsinaga9c@gmail.com>
2021-06-17 01:30:35 +0700

    improve ui

--- app/src/main/java/com/dicoding/upcomingmovies2021/ui/fragments/DetailFragment.kt
+++ app/src/main/java/com/dicoding/upcomingmovies2021/ui/fragments/DetailFragment.kt
@@ -3,18 +3,16 @@ package com.dicoding.upcomingmovies2021.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import coil.load
 import coil.size.Scale
-import coil.transform.CircleCropTransformation
-import coil.transform.GrayscaleTransformation
 import com.dicoding.upcomingmovies2021.R
 import com.dicoding.upcomingmovies2021.databinding.FragmentDetailBinding
 import com.dicoding.upcomingmovies2021.ui.viewmodel.DetailViewModel
 import com.dicoding.upcomingmovies2021.utils.Constants.API_BACKDROP_PATH
-import com.dicoding.upcomingmovies2021.utils.Constants.API_POSTER_PATH
 import com.dicoding.upcomingmovies2021.utils.TypeFilm
 import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,29 +39,35 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     private fun populateDetailMovie() {
         viewModel.movieDetail.observe(viewLifecycleOwner, { detailMovie ->
-            Log.d("movieDb", detailMovie.title)
+
             binding.apply {
-                tvTitle.text = detailMovie.title
-                tvReleaseDate.text = detailMovie.releaseDate
-                contentDescription.text = detailMovie.overview
+                (requireActivity() as AppCompatActivity).setSupportActionBar(toolbarDetailFilm)
+                toolbarDetailFilm.title = detailMovie.title
+                toolbarDetailFilm.setNavigationIcon(R.drawable.ic_back)
+                toolbarDetailFilm.setNavigationOnClickListener {
+                    activity?.onBackPressed()
+                }
+
+                tvTitleDetailFilm.text = detailMovie.title
+                tvReleaseDateDetailFilm.text = detailMovie.releaseDate
+                tvDescriptionDetailFilm.text = detailMovie.overview
 
                 val imgPosterUrl = API_BACKDROP_PATH + detailMovie.backdropPath
-                imgPoster.load(imgPosterUrl){
+                imgPosterDetailFilm.load(imgPosterUrl) {
                     scale(Scale.FILL)
                     placeholder(R.drawable.ic_placeholder)
+                    error(R.drawable.ic_broken_image)
+                    fallback(R.drawable.ic_placeholder)
                     crossfade(true)
                     crossfade(400)
-                    transformations(GrayscaleTransformation())
                 }
 
-                val imgIconUrl = API_POSTER_PATH + detailMovie.posterPath
-                imgIcon.load(imgIconUrl){
-                    placeholder(R.drawable.ic_placeholder)
-                    transformations(CircleCropTransformation())
+                for (genre in detailMovie.genres) {
+                    createChip(genre.name, 0)
                 }
 
-                for (genre in detailMovie.genres){
-                    createChip(genre.name)
+                for (company in detailMovie.productionCompanies) {
+                    createChip(company.name, 1)
                 }
             }
         })
@@ -71,36 +75,41 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     private fun populateTvShow() {
         viewModel.tvShowDetail.observe(viewLifecycleOwner, { detailTvShow ->
-            Log.d("movieDb", detailTvShow.originalName)
 
             binding.apply {
-                tvTitle.text = detailTvShow.originalName
-                tvReleaseDate.text = detailTvShow.firstAirDate
-                contentDescription.text = detailTvShow.overview
+                (requireActivity() as AppCompatActivity).setSupportActionBar(toolbarDetailFilm)
+                toolbarDetailFilm.title = detailTvShow.originalName
+                toolbarDetailFilm.setNavigationIcon(R.drawable.ic_back)
+                toolbarDetailFilm.setNavigationOnClickListener {
+                    activity?.onBackPressed()
+                }
+
+                tvTitleDetailFilm.text = detailTvShow.originalName
+                tvReleaseDateDetailFilm.text = detailTvShow.firstAirDate
+                tvDescriptionDetailFilm.text = detailTvShow.overview
 
                 val imgPosterUrl = API_BACKDROP_PATH + detailTvShow.backdropPath
-                imgPoster.load(imgPosterUrl){
+                imgPosterDetailFilm.load(imgPosterUrl) {
                     scale(Scale.FILL)
                     placeholder(R.drawable.ic_placeholder)
+                    error(R.drawable.ic_broken_image)
+                    fallback(R.drawable.ic_placeholder)
                     crossfade(true)
                     crossfade(400)
-                    transformations(GrayscaleTransformation())
                 }
 
-                val imgIconUrl = API_POSTER_PATH + detailTvShow.posterPath
-                imgIcon.load(imgIconUrl){
-                    placeholder(R.drawable.ic_placeholder)
-                    transformations(CircleCropTransformation())
+                for (genre in detailTvShow.genres) {
+                    createChip(genre.name, 0)
                 }
 
-                for (genre in detailTvShow.genres){
-                    createChip(genre.name)
+                for (company in detailTvShow.productionCompanies) {
+                    createChip(company.name, 1)
                 }
             }
         })
     }
 
-    private fun createChip(txt: String) {
+    private fun createChip(txt: String, type: Int) {
         val chip = Chip(context)
         chip.apply {
             text = txt
@@ -108,7 +117,8 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
             isCloseIconVisible = false
             isClickable = false
             isCheckable = false
-            binding.chipGenre.addView(chip)
+            if (type == 0) binding.chipGenreDetailFilm.addView(chip)
+            else if (type == 1) binding.chipProductionCompanyDetailFilm.addView(chip)
         }
     }
 }
